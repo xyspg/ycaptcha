aec6cd3537202478c5b90c1f6e3e5a3d3f830898
Jiaming Kang <42668274+xyspg@users.noreply.github.com>
2026-03-25 23:36:45 -0400

    fix: incorrect dark mode on production

--- hooks/use-mount-effect.ts
+++ hooks/use-mount-effect.ts
@@ -1,5 +1,5 @@
 // biome-ignore lint/style/noRestrictedImports: this is the wrapper itself
-import { useEffect } from "react";
+import { type EffectCallback, useEffect } from "react";
 
-export const useMountEffect = (callback: () => undefined | (() => void)) =>
+export const useMountEffect = (callback: EffectCallback) =>
 	useEffect(callback, []);
